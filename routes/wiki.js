317d8c3b22f82bc3eaf60dca4dbde38a835af2e6
Andrew Pratt <apratt957@gmail.com>
2019-09-26 15:52:14 -0500

    finished showing a wiki page section

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const addpage = require('../views/addPage')
 const { Page } = require("../models");
+const wikipage = require('../views/wikipage')
+const mainpage = require('../views/main')
 const router = express.Router()
 
 
 
 
-router.get('/', (req,res)=>{
-  res.send('this is all the wiki pages')
+router.get('/', async (req,res)=>{
+  const pages = await Page.findAll({
+    raw: true
+  });
+  res.send(mainpage(pages));
 })
 
 router.post('/', async (req,res, next)=>{
@@ -22,15 +27,24 @@ router.post('/', async (req,res, next)=>{
   })
   try{
     await page.save();
-    res.redirect('/');
+    res.redirect(`/wiki/${page.slug}`);
   } catch (err) {
     console.error(err);
   }
-  console.log(page);
+  // console.log(page);
 })
 
 router.get('/add', (req,res)=>{
   res.send(addpage())
 })
+
+router.get('/:slug', async (req, res, next) => {
+  try { 
+  const targetPage = await Page.findOne({ where: {slug: req.params.slug} });
+  res.send(wikipage(targetPage));
+  } catch(err) {
+    console.error(err);
+  }
+});
 
 module.exports = router
